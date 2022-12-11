6cee82cdbc2e8b33929061005562d70d54bb2f7d
smarcitllach <smarci01@ucm.es>
2022-12-11 15:37:06 +0100

    mejora averagePrice

--- PROJECT/averagePrice/average_price.py
+++ PROJECT/averagePrice/average_price.py
@@ -14,9 +14,14 @@ path = "Airbnb barcelona.csv"
 
 df = spark.read.option('header', 'true').csv(path)
 
-df = df.select(df['zipcode'], df['price'], f.regexp_replace(f.col('price'), '[\$]', '').alias('priceAux'), df['review_scores_rating'])
-df = df.withColumn('priceAux', expr("substring(priceAux, 1, length(priceAux)-4)"))
-df.show()
-df = df.groupBy(df['zipcode']).agg(f.avg(df['priceAux']).alias('averagePrice')).sort('zipcode')
+dfPrice = df.select(df['zipcode'], df['price'])
+#dfPrice = dfPrice.withColumn('zipcode', f.regexp_extract_all(f.col('zipcode'), '8[0-9]{3}'))
 
-df.show()
+dfPrice = dfPrice.withColumn('price', f.regexp_replace(f.col('price'), '[\$]', '').alias('priceAux'))
+dfPrice = dfPrice.withColumn('priceAux', expr("substring(price, 0, length(price)-4)"))
+dfPrice = dfPrice.groupBy(dfPrice['zipcode']).agg(f.avg(dfPrice['priceAux']).alias('averagePrice'))
+dfPrice.show(80)
+
+dfRating = df.select(df['zipcode'], df['review_scores_rating'])
+dfRating = dfRating.groupBy(df['zipcode']).agg(f.avg(dfRating['review_scores_rating']).alias('averageRating'))
+dfRating.show(80)
