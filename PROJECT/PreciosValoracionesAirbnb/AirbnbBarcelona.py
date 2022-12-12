cb0b99342d4484d2b942590b3baa50c3e9a704a3
smarcitllach <smarci01@ucm.es>
2022-12-12 18:09:07 +0100

    scripts acabados, excel hechos y tal

--- PROJECT/PreciosValoracionesAirbnb/AirbnbBarcelona.py
+++ PROJECT/PreciosValoracionesAirbnb/AirbnbBarcelona.py
@@ -18,45 +18,64 @@ df = spark.read.option('header', 'true').csv(path)
 dfPrice = df.select(df['zipcode'], df['price'])
 dfPrice = dfPrice.na.drop()
 dfPrice = dfPrice.withColumn('zipcode', f.regexp_extract(f.col('zipcode'), '8[0-9]{3}', 0))
-#dfPrice = dfPrice.filter(dfPrice['price'].rlike('8[0-9]{3}'))
 dfPrice = dfPrice.withColumn('price', f.regexp_replace(f.col('price'), '[\$]', '').alias('priceAux'))
 dfPrice = dfPrice.withColumn('priceAux', expr("substring(price, 0, length(price)-4)"))
 dfPrice = dfPrice.groupBy(dfPrice['zipcode']).agg(f.avg(dfPrice['priceAux']).alias('averagePrice'))
-dfPrice.sort(dfPrice['averagePrice']).show()
+dfPrice = dfPrice.sort(dfPrice['averagePrice'])
+#dfPrice.show()
 
 #EXCEL
-dfPrice.toPandas().to_excel("averagePriceBarcelonaZipcode.csv", index=False)
+dfPrice.toPandas().to_excel("averagePriceBarcelonaZipcode.xlsx", index=False)
+
+topPriceBarcelonaZipcode = dfPrice.toPandas().iloc[:10, :]
+topPriceBarcelonaZipcode.to_excel("topPriceBarcelonaZipcode.xlsx", index=False)
 
 #PUNTUACION MEDIA POR CODIGO POSTAL (DESCENDENTE)
 dfRating = df.select(df['zipcode'], df['review_scores_rating'])
 dfRating = dfRating.na.drop()
 dfRating = dfRating.withColumn('zipcode', f.regexp_extract(f.col('zipcode'), '8[0-9]{3}', 0))
 dfRating = dfRating.groupBy(dfRating['zipcode']).agg(f.avg(dfRating['review_scores_rating']).alias('averageRating'))
-dfRating.sort(f.desc(dfRating['averageRating'])).show()
+dfRating = dfRating.sort(f.desc(dfRating['averageRating']))
+#dfRating.show()
 
 #EXCEL
-dfRating.toPandas().to_excel("averageRatingBarcelonaZipcode.csv", index=False)
+dfRating.toPandas().to_excel("averageRatingBarcelonaZipcode.xlsx", index=False)
+
+topRatingBarcelonaZipcode = dfRating.toPandas().iloc[:10, :]
+topRatingBarcelonaZipcode.to_excel("topRatingBarcelonaZipcode.xlsx", index=False)
 
 #PRECIO MEDIO POR BARRIO (ASCENDENTE)
 dfPrice2 = df.select(df['neighbourhood_cleansed'], df['price'])
 dfPrice2 = dfPrice2.na.drop()
-dfPrice2 = dfPrice2.withColumn('neighbourhood_cleansed', f.regexp_extract(f.col('neighbourhood_cleansed'), '^([^0-9]*)$', 0))
+dfPrice2 = dfPrice2.withColumnRenamed('neighbourhood_cleansed', 'neighbourhood')
+dfPrice2 = dfPrice2.withColumn('neighbourhood', f.regexp_extract(f.col('neighbourhood'), '^([^0-9]*)$', 0))
 dfPrice2 = dfPrice2.withColumn('price', f.regexp_replace(f.col('price'), '[\$]', '').alias('priceAux'))
 dfPrice2 = dfPrice2.withColumn('priceAux', expr("substring(price, 0, length(price)-4)"))
-dfPrice2 = dfPrice2.groupBy(dfPrice2['neighbourhood_cleansed']).agg(f.avg(dfPrice2['priceAux']).alias('averagePrice'))
+dfPrice2 = dfPrice2.groupBy(dfPrice2['neighbourhood']).agg(f.avg(dfPrice2['priceAux']).alias('averagePrice'))
 dfPrice2 = dfPrice2.na.drop()
-dfPrice2.sort(dfPrice2['averagePrice']).show(80)
+dfPrice2 = dfPrice2.sort(dfPrice2['averagePrice'])
+#dfPrice2.show()
 
 #EXCEL
-dfPrice2.toPandas().to_excel("averagePriceBarcelonaNeighbourhoods.csv", index=False)
+dfPrice2.toPandas().to_excel("averagePriceBarcelonaNeighbourhoods.xlsx", index=False)
+
+topPriceBarcelonaNeighbourhoods = dfPrice2.toPandas().iloc[:10, :]
+topPriceBarcelonaNeighbourhoods.to_excel("topPriceBarcelonaNeighbourhoods.xlsx", index=False)
 
 #PUNTUACION MEDIA POR BARRIO (DESCENDENTE)
 dfRating2 = df.select(df['neighbourhood_cleansed'], df['review_scores_rating'])
 dfRating2 = dfRating2.na.drop()
-dfRating2 = dfRating2.withColumn('neighbourhood_cleansed', f.regexp_extract(f.col('neighbourhood_cleansed'), '^([^0-9]*)$', 0))
-dfRating2 = dfRating2.groupBy(dfRating2['neighbourhood_cleansed']).agg(f.avg(dfRating2['review_scores_rating']).alias('averageRating'))
+dfRating2 = dfRating2.withColumnRenamed('neighbourhood_cleansed', 'neighbourhood')
+dfRating2 = dfRating2.withColumn('neighbourhood', f.regexp_extract(f.col('neighbourhood'), '^([^0-9]*)$', 0))
+dfRating2 = dfRating2.groupBy(dfRating2['neighbourhood']).agg(f.avg(dfRating2['review_scores_rating']).alias('averageRating'))
 dfRating2 = dfRating2.na.drop()
-dfRating2.sort(f.desc(dfRating2['averageRating'])).show(80)
+dfRating2 = dfRating2.sort(f.desc(dfRating2['averageRating']))
+#dfRating2.show()
 
 #EXCEL
-dfRating2.toPandas().to_excel("averageRatingBarcelonaNeighbourhoods.csv", index=False)
+dfRating2.toPandas().to_excel("averageRatingBarcelonaNeighbourhoods.xlsx", index=False)
+
+topRatingBarcelonaNeighbourhoods = dfRating2.toPandas().iloc[:10, :]
+topRatingBarcelonaNeighbourhoods.to_excel("topRatingBarcelonaNeighbourhoods.xlsx", index=False)
+
+print('---------------SUCCESS---------------')
--- PROJECT/PreciosValoracionesAirbnb/AirbnbMadrid.py
+++ PROJECT/PreciosValoracionesAirbnb/AirbnbMadrid.py
@@ -17,19 +17,31 @@ df = spark.read.option('header', 'true').csv(path)
 #PRECIO MEDIO POR BARRIO (ASCENDENTE)
 dfPrice = df.select(df['neighbourhood_cleansed'], df['price'])
 dfPrice = dfPrice.na.drop()
+dfPrice = dfPrice.withColumnRenamed('neighbourhood_cleansed', 'neighbourhood')
 dfPrice = dfPrice.withColumn('price', f.regexp_replace(f.col('price'), '[\$]', '').alias('priceAux'))
 dfPrice = dfPrice.withColumn('priceAux', expr("substring(price, 0, length(price)-4)"))
-dfPrice = dfPrice.groupBy(dfPrice['neighbourhood_cleansed']).agg(f.avg(dfPrice['priceAux']).alias('averagePrice'))
-dfPrice.sort(dfPrice['averagePrice']).show(80)
+dfPrice = dfPrice.groupBy(dfPrice['neighbourhood']).agg(f.avg(dfPrice['priceAux']).alias('averagePrice'))
+dfPrice = dfPrice.sort(dfPrice['averagePrice'])
+#dfPrice.show(80)
 
 #EXCEL
 dfPrice.toPandas().to_excel("averagePriceMadridNeighbourhoods.xlsx", index=False)
 
+topPriceMadridNeighbourhoods = dfPrice.toPandas().iloc[:10, :]
+topPriceMadridNeighbourhoods.to_excel("topPriceMadridNeighbourhoods.xlsx", index=False)
+
 #PUNTUACION MEDIA POR BARRIO (DESCENDENTE)
 dfRating = df.select(df['neighbourhood_cleansed'], df['review_scores_rating'])
 dfRating = dfRating.na.drop()
-dfRating = dfRating.groupBy(dfRating['neighbourhood_cleansed']).agg(f.avg(dfRating['review_scores_rating']).alias('averageRating'))
-dfRating.sort(f.desc(dfRating['averageRating'])).show(80)
+dfRating = dfRating.withColumnRenamed('neighbourhood_cleansed', 'neighbourhood')
+dfRating = dfRating.groupBy(dfRating['neighbourhood']).agg(f.avg(dfRating['review_scores_rating']).alias('averageRating'))
+dfRating = dfRating.sort(f.desc(dfRating['averageRating']))
+#dfRating.show(80)
 
 #EXCEL
 dfRating.toPandas().to_excel("averageRatingMadridNeighbourhoods.xlsx", index=False)
+
+topRatingMadridNeighbourhoods = dfRating.toPandas().iloc[:10, :]
+topRatingMadridNeighbourhoods.to_excel("topRatingMadridNeighbourhoods.xlsx", index=False)
+
+print('---------------SUCCESS---------------')
